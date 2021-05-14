38ea664e18bd467b0cdc3f401bbd0def1758cf14
RyanAugust <rad19206@yahoo.com>
2021-05-14 10:57:11 -0700

    utilities for import of hrv and weight metrics

---
+++ utils/hrv_import.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # HRV
+# ___
+# ## Import Records
+import datetime
+import pytz
+import time
+import os
+from pathlib import Path
+import pandas as pd
+import json
+
+hrv_save_dir = '/Users/ryanduecker/Dropbox/Apps/HRV4Training/'
+
+for a,b,c in os.walk(hrv_save_dir):
+    csvs = []
+for file in c:
+    if '.csv' in file:
+        csvs.append(file)
+
+## Fix dates to search for up to date file
+date_dict = {}
+for file in csvs:
+    year, month, day = [int(val) for val in file.split('_')[:3]]
+    if year in date_dict.keys():
+        if month in date_dict[year].keys():
+            date_dict[year][month].append(day)
+        else:
+            date_dict[year][month] = [day]
+    else:
+        date_dict[year] = {month:[day]}
+max_year = max(list(date_dict.keys()))
+max_month = max(list(date_dict[max_year].keys()))
+max_day = max(date_dict[max_year][max_month])
+max_date = '{}_{}_{}'.format(max_year,max_month,max_day)
+
+
+target_file = '{}{}_myMeasurements.csv'.format(hrv_save_dir,max_date)
+
+hrv_df = pd.read_csv(target_file)
+hrv_df = hrv_df.replace('-',pd.NA).dropna(subset=['rMSSD']).copy()
+
+
+hrv_df['date'] = hrv_df['date'].apply(lambda x: x.replace('0000','0800'))
+hrv_df['date'] = pd.to_datetime(hrv_df['date'])
+hrv_df['timestamp_measurement'] = hrv_df['timestamp_measurement'].apply(lambda x: x.replace('0000','0800'))
+hrv_df['timestamp_measurement'] = pd.to_datetime(hrv_df['timestamp_measurement'])
+
+
+## Load Measures
+home = str(Path.home())
+athlete = 'Ryan Duecker'
+hrv_path = '{home}/Library/Mobile Documents/com~apple~CloudDocs/GoldenCheetah/{athlete}/config/hrvmeasures.json'.format(home=home,athlete=athlete)
+
+with open(hrv_path, 'r') as f:
+    original = json.loads(f.read())
+    f.close()
+measures = original['measures']
+version = original['version']
+
+latest_date = datetime.datetime.fromtimestamp(measures[-1]['when'],tz=pytz.timezone('US/Pacific'))
+
+needed_cols = list(measures[0].keys())
+
+## Prep data
+new_hrv_data = hrv_df[hrv_df['timestamp_measurement'] > latest_date].copy()
+print('{} new HRV records'.format(new_hrv_data.shape[0]))
+rename_dict = {'AVNN':'avnn',
+                'HF':'hf',
+                'HR':'hr',
+                'LF':'lf',
+                'pNN50':'pnn50',
+                'HRV4T_Recovery_Points':'recovery_points',
+                'rMSSD':'rmssd',
+                'SDNN':'sdnn',
+                'timestamp_measurement':'when'}
+
+## Data Hardsets
+static_data = {'originalsource':'HRV4Training',
+               'comment':'',
+               'source':3}
+
+for col, val in static_data.items():
+    new_hrv_data[col] = val
+
+new_hrv_data['timestamp_measurement'] = new_hrv_data['timestamp_measurement'].apply(lambda x: int(time.mktime(x.timetuple())))
+
+
+## Build Payload
+for col in list(rename_dict.keys()):
+    try:
+        new_hrv_data[col] = new_hrv_data[col].astype(float)
+    except:
+        pass
+
+new_json = json.loads(new_hrv_data.rename(columns=rename_dict)[needed_cols].to_json(orient='records'))
+[measures.append(record) for record in new_json]
+payload = {'measures':measures,
+          'version':version}
+
+## Dump Data to File
+with open(hrv_path, 'w') as f:
+    f.write(json.dumps(payload))
+    f.close()
---
+++ utils/measures_import.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import requests
+import json
+import pandas as pd
+import gspread
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from oauth2client.service_account import ServiceAccountCredentials
+
+athlete = 'Ryan Duecker'
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('/Users/ryanduecker/projects/triathlon/golden-cheetah-upload-ec5eca7e3cb2.json', scope)
+
+client = gspread.authorize(creds)
+spreadsheet = client.open_by_key(key='12HAyrDDvdeWvnLYsAX9PPrk-Jrx5Ax8gYKQOw6nv0EQ')
+
+## Weight
+weight = spreadsheet.worksheets()[1]
+weight_df = pd.DataFrame(weight.get_all_records())
+weight_df['Date'] = pd.to_datetime(weight_df['Date'])
+
+## Load Measures
+import datetime
+import time
+from pathlib import Path
+home = str(Path.home())
+
+measures_path = '{home}/Library/Mobile Documents/com~apple~CloudDocs/GoldenCheetah/{athlete}/config/bodymeasures.json'.format(home=home,athlete=athlete)
+with open(measures_path, 'r') as f:
+    original = json.loads(f.read())
+    f.close()
+
+## Extract parts of file structure
+measures = original['measures']
+version = original['version']
+## know when the latest date was
+latest_date = datetime.datetime.fromtimestamp(pd.DataFrame(measures)['when'].max())
+
+# ## Prep data
+needed_cols = list(original['measures'][0].keys())
+new_cols = []
+rename_dict = {'Weight':'weightkg',
+               'Fat':'fatpercent',
+               'Date':'when',
+               'Data Source':'originalsource'}
+
+# ## Restrict to only new data 
+weight_df['Fat'] = weight_df['Fat'].bfill()
+new_data = weight_df[weight_df['Date'] > latest_date].copy()
+print('{} new weight records'.format(new_data.shape[0]))
+# New columns
+new_data['fatkg'] = new_data['Weight']*new_data['Fat']/100
+new_data['fatkg'] = new_data['fatkg'].apply(lambda x: round(x,2))
+new_cols.append('fatkg')
+
+# Data Hardsets
+static_data = {'boneskg':3.49,
+               'comment':'',
+               'leankg':0,
+               'musclekg':0,
+              'source':3}
+for col, val in static_data.items():
+    new_data[col] = val
+
+new_data['Date'] = new_data['Date'].apply(lambda x: int(time.mktime(x.timetuple())))
+
+# ## Build Payload
+new_json = json.loads(new_data.rename(columns=rename_dict)[needed_cols].to_json(orient='records'))
+[measures.append(record) for record in new_json]
+payload = {'measures':measures,
+          'version':version}
+
+# ## Dump Data to File
+with open(measures_path, 'w') as f:
+    f.write(json.dumps(payload))
+    f.close()
